ae5499fa945844338ce4270b1d82a3f57cebe4b1
Arjun Mulchandani <92887874+FloopyFlop@users.noreply.github.com>
2023-07-11 15:52:47 -0700

    Moved functions over from sudoku_lib as in the template

--- sp1/sudoku.py
+++ sp1/sudoku.py
@@ -1,14 +1,83 @@
 import sudoku_lib as sl
 
-sudokus = [[[3, 2, 0, 9, 6, 0, 0, 0, 4], [8, 4, 1, 7, 5, 2, 3, 9, 6], [9, 5, 0, 3, 4, 8, 0, 7, 0], [0, 6, 5, 4, 1, 3, 9, 2, 8], [2, 9, 8, 5, 7, 6, 1, 4, 3], [1, 3, 4, 8, 2, 9, 7, 0, 5], [5, 7, 9, 1, 0, 4, 0, 8, 2], [6, 8, 3, 2, 9, 5, 0, 1, 7], [4, 1, 2, 6, 0, 0, 5, 3, 0]], [[4, 0, 5, 9, 0, 0, 0, 3, 0], [8, 7, 1, 0, 0, 0, 5, 6, 9], [3, 6, 9, 0, 5, 0, 0, 0, 2], [7, 0, 2, 5, 0, 4, 1, 8, 6], [5, 4, 8, 2, 6, 1, 9, 7, 3], [0, 1, 6, 8, 0, 7, 2, 5, 4], [6, 0, 0, 1, 2, 0, 3, 9, 7], [1, 9, 7, 4, 8, 3, 6, 2, 5], [2, 5, 0, 6, 7, 9, 8, 4, 1]], [[5, 0, 6, 2, 0, 4, 1, 7, 0], [0, 3, 2, 1, 7, 8, 0, 0, 9], [1, 0, 7, 0, 6, 9, 0, 3, 2], [0, 1, 3, 9, 4, 0, 8, 0, 7], [7, 5, 9, 3, 0, 2, 6, 1, 4], [8, 0, 4, 6, 0, 7, 3, 9, 5], [0, 0, 1, 8, 0, 3, 7, 0, 6], [0, 0, 0, 4, 0, 6, 2, 8, 1], [2, 0, 8, 0, 5, 1, 0, 4, 3]], [[0, 0, 4, 6, 7, 2, 0, 0, 5], [2, 0, 0, 8, 9, 0, 4, 3, 0], [5, 0, 0, 0, 0, 3, 7, 2, 6], [0, 0, 7, 0, 5, 0, 1, 4, 0], [9, 5, 1, 3, 6, 0, 2, 0, 0], [4, 2, 8, 0, 0, 7, 0, 5, 3], [1, 6, 0, 4, 8, 9, 3, 0, 2], [8, 0, 2, 0, 3, 6, 0, 1, 0], [7, 4, 3, 5, 2, 1, 9, 6, 8]], [[0, 0, 0, 8, 4, 0, 1, 7, 5], [0, 8, 4, 0, 9, 7, 0, 0, 6], [3, 0, 1, 2, 6, 0, 4, 0, 0], [0, 9, 2, 6, 5, 0, 7, 0, 8], [7, 5, 3, 9, 2, 8, 6, 0, 0], [0, 1, 0, 7, 0, 0, 0, 0, 0], [0, 6, 0, 4, 7, 9, 8, 1, 3], [1, 4, 9, 3, 8, 0, 5, 6, 7], [8, 3, 0, 0, 0, 6, 2, 0, 4]], [[4, 0, 9, 3, 2, 5, 6, 7, 0], [5, 1, 7, 8, 0, 0, 3, 0, 0], [2, 3, 0, 7, 9, 0, 0, 4, 0], [0, 6, 0, 1, 8, 0, 0, 2, 0], [7, 4, 8, 0, 0, 0, 0, 6, 0], [1, 0, 0, 6, 0, 7, 9, 0, 0], [6, 0, 0, 0, 0, 3, 8, 0, 0], [8, 0, 1, 4, 7, 9, 2, 0, 0], [3, 0, 2, 0, 0, 0, 4, 1, 7]], [[0, 5, 8, 0, 4, 0, 6, 1, 7], [0, 7, 9, 1, 8, 5, 4, 3, 0], [0, 1, 0, 6, 0, 0, 0, 0, 0], [9, 0, 5, 3, 0, 0, 0, 7, 8], [0, 0, 0, 0, 0, 0, 0, 9, 0], [8, 3, 0, 0, 5, 0, 2, 6, 0], [1, 8, 6, 5, 0, 2, 0, 0, 3], [3, 0, 0, 0, 0, 0, 9, 0, 1], [0, 0, 0, 4, 3, 0, 0, 2, 0]], [[0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 1, 0, 9, 6, 2, 0, 0], [6, 8, 0, 0, 0, 1, 0, 9, 3], [9, 4, 0, 6, 0, 3, 0, 0, 0], [0, 6, 0, 9, 0, 0, 8, 0, 0], [0, 0, 5, 0, 0, 4, 0, 3, 9], [0, 5, 4, 8, 0, 2, 0, 0, 0], [1, 9, 6, 0, 0, 0, 0, 0, 8], [0, 2, 7, 0, 0, 9, 4, 5, 0]], [[3, 0, 8, 0, 0, 0, 0, 0, 0], [6, 0, 7, 3, 1, 0, 8, 4, 0], [0, 9, 1, 0, 0, 0, 0, 0, 7], [0, 4, 0, 0, 2, 5, 0, 0, 0], [0, 6, 9, 4, 7, 0, 0, 3, 0], [5, 7, 0, 9, 0, 6, 0, 0, 1], [0, 0, 4, 7, 6, 0, 5, 2, 0], [0, 0, 6, 0, 0, 3, 9, 0, 4], [0, 0, 0, 0, 0, 0, 0, 0, 8]], [[7, 9, 0, 0, 0, 1, 0, 2, 0], [5, 0, 0, 4, 0, 0, 9, 0, 0], [0, 1, 6, 0, 2, 9, 7, 3, 0], [9, 0, 0, 7, 4, 2, 0, 5, 3], [2, 4, 0, 0, 0, 0, 0, 0, 0], [8, 5, 0, 0, 0, 0, 0, 9, 0], [1, 0, 0, 2, 0, 3, 0, 0, 0], [0, 0, 0, 0, 1, 4, 0, 0, 0], [0, 0, 5, 9, 0, 7, 3, 0, 0]]]
+def sudokuCoordinates(coords): # STUDENT MADE
+    return [int(coords[0]), int(coords[2])]
+
+def is_valid_move(sudoku, row, col, num): # STUDENT MADE
+    # Check if 'num' is already present in the row or column
+    for i in range(9):
+        if sudoku[row][i] == num:
+            return False
+        if sudoku[i][column] == num:
+            return False
+
+    # Check if 'num' is already present in the 3x3 grid
+    """
+    1.
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+
+    2.
+    if (row == 0 or row == 1 or row == 2):
+        start_row = 0
+    elif (row == 3 or row == 4 or row == 5):
+        start_row = 3
+    elif (row == 6 or row == 7 or row == 8):
+        start_row = 6
+
+    if (column == 0 or column == 1 or column == 2):
+        start_column = 0
+    elif (column == 3 or column == 4 or column == 5):
+        start_column = 3
+    elif (column == 6 or column == 7 or column == 8):
+        start_column = 6
+
+    3.
+    indexToStart = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 3,
+        4: 3,
+        5: 3,
+        6: 6,
+        7: 6,
+        8: 6,
+    }
+    start_column = indexToStart[column]
+    start_row = indexToStart[row]
+    """
+    
+    indexToStart = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 3,
+        4: 3,
+        5: 3,
+        6: 6,
+        7: 6,
+        8: 6,
+    }
+  
+    start_column = indexToStart[column]
+    start_row = indexToStart[row]
+    for i in range(3):
+        for j in range(3):
+            if sudoku[start_row + i][start_col + j] == num:
+                return False
+
+    return True
+
+sudoku = sl.sudokus[9]
 
 while True:
     sl.display_sudoku(sudoku) # Provided
 
     num = int(input("What number would you like to place?: "))
-    position = sl.sudokuCoordinates(input("Where woudld you like to place it? [X,Y]: "))
+    position = sudokuCoordinates(input("Where woudld you like to place it? [X,Y]: "))
     
-    if sl.is_valid_move(sudoku, position[0], position[1], num):
+    if is_valid_move(sudoku, position[0], position[1], num):
         sudoku[position[0]][position[1]] = num
     else:
         print("\nThat was not a valid move, make another!\n")
